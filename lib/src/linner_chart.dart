7a8d47f5840df127db7b2076403c4681728aa996
ali <amotie@outlook.com>
2022-03-24 04:38:00 +0200

    Adding Stroke Width

--- lib/src/linner_chart.dart
+++ lib/src/linner_chart.dart
@@ -1,13 +1,14 @@
-
 import 'package:chart/src/models/chart_model.dart';
 import 'package:chart/src/view/chart_painter.dart';
 import 'package:flutter/material.dart';
 
 class LinnerChat extends StatelessWidget {
   final List<ChartModel>? charts;
+  final double strokeWidth;
   const LinnerChat({
     Key? key,
     @required this.charts,
+    this.strokeWidth = 20,
   }) : super(key: key);
 
   @override
@@ -17,6 +18,7 @@ class LinnerChat extends StatelessWidget {
       painter: ChartPainter(
         charts: charts,
         backgroundColor: Colors.grey,
+        strokeWidth: strokeWidth,
       ),
     );
   }
