6e3eac8ad9202a7e51d1f6b9b396586349f6ffb7
ali <amotie@outlook.com>
2022-03-24 14:51:51 +0200

    if persinte equel zero dont draw

--- lib/src/view/chart_painter.dart
+++ lib/src/view/chart_painter.dart
@@ -20,40 +20,42 @@ class ChartPainter extends CustomPainter {
       ..style = PaintingStyle.stroke;
     canvas.drawLine(const Offset(0, 0), Offset(size.width, 0), bar);
     for (int index = 0; index < charts!.length; index++) {
-      if (index == 0) {
-        final item = Paint()
-          ..color = charts![index].color
-          ..strokeCap = StrokeCap.round
-          ..style = PaintingStyle.stroke
-          ..strokeWidth = strokeWidth
-          ..style = PaintingStyle.stroke;
-        var endpoint = size.width * charts![index].persintge;
-        canvas.drawLine(const Offset(0, 0), Offset(endpoint, 0), item);
-        persintge += charts![index].persintge;
-      } else if (index == charts!.length - 1) {
-        final item2 = Paint()
-          ..color = charts![index].color
-          ..strokeCap = StrokeCap.round
-          ..style = PaintingStyle.stroke
-          ..strokeWidth = strokeWidth
-          ..style = PaintingStyle.stroke;
-        var endpoint = size.width * (charts![index].persintge + persintge);
+      if (charts![index].persintge != 0) {
+        if (index == 0) {
+          final item = Paint()
+            ..color = charts![index].color
+            ..strokeCap = StrokeCap.round
+            ..style = PaintingStyle.stroke
+            ..strokeWidth = strokeWidth
+            ..style = PaintingStyle.stroke;
+          var endpoint = size.width * charts![index].persintge;
+          canvas.drawLine(const Offset(0, 0), Offset(endpoint, 0), item);
+          persintge += charts![index].persintge;
+        } else if (index == charts!.length - 1) {
+          final item2 = Paint()
+            ..color = charts![index].color
+            ..strokeCap = StrokeCap.round
+            ..style = PaintingStyle.stroke
+            ..strokeWidth = strokeWidth
+            ..style = PaintingStyle.stroke;
+          var endpoint = size.width * (charts![index].persintge + persintge);
 
-        canvas.drawLine(
-            Offset(persintge * size.width, 0), Offset(endpoint, 0), item2);
-        persintge += charts![index].persintge;
-      } else {
-        final item2 = Paint()
-          ..color = charts![index].color
-          ..strokeCap = StrokeCap.round
-          ..style = PaintingStyle.stroke
-          ..strokeWidth = strokeWidth
-          ..style = PaintingStyle.stroke;
-        var endpoint = size.width * (charts![index].persintge + persintge);
+          canvas.drawLine(
+              Offset(persintge * size.width, 0), Offset(endpoint, 0), item2);
+          persintge += charts![index].persintge;
+        } else {
+          final item2 = Paint()
+            ..color = charts![index].color
+            ..strokeCap = StrokeCap.round
+            ..style = PaintingStyle.stroke
+            ..strokeWidth = strokeWidth
+            ..style = PaintingStyle.stroke;
+          var endpoint = size.width * (charts![index].persintge + persintge);
 
-        canvas.drawLine(
-            Offset(persintge * size.width, 0), Offset(endpoint, 0), item2);
-        persintge += charts![index].persintge;
+          canvas.drawLine(
+              Offset(persintge * size.width, 0), Offset(endpoint, 0), item2);
+          persintge += charts![index].persintge;
+        }
       }
     }
   }
