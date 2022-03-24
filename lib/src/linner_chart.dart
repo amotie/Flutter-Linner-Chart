1c3369b9f1cc1925c26eee7963e3df3e8f54cddd
ali <amotie@outlook.com>
2022-03-24 05:05:19 +0200

    backgournd Color

--- lib/src/linner_chart.dart
+++ lib/src/linner_chart.dart
@@ -5,10 +5,12 @@ import 'package:flutter/material.dart';
 class LinnerChat extends StatelessWidget {
   final List<ChartModel>? charts;
   final double strokeWidth;
+  final Color backgroundColor;
   const LinnerChat({
     Key? key,
     @required this.charts,
     this.strokeWidth = 20,
+    this.backgroundColor = Colors.grey,
   }) : super(key: key);
 
   @override
@@ -17,7 +19,7 @@ class LinnerChat extends StatelessWidget {
       size: Size(MediaQuery.of(context).size.width - 30, 0),
       painter: ChartPainter(
         charts: charts,
-        backgroundColor: Colors.grey,
+        backgroundColor: backgroundColor,
         strokeWidth: strokeWidth,
       ),
     );
